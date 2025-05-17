aec1d8897f3d810915946f650747da67e73b85d2
mawi-delosreyes <mwldelosreyes13@gmail.com>
2025-05-17 16:48:19 +0800

    Updated SQL queries

--- Database Schema/Harvest.sql
+++ Database Schema/Harvest.sql
@@ -1,9 +1,7 @@
 CREATE TABLE User (
 	user_id INTEGER(11) NOT NULL AUTO_INCREMENT,
 	name VARCHAR(50),
-	api_key VARCHAR(50) NOT NULL,
-	take_profit INTEGER(3) NOT NULL,
-	stop_loss INTEGER(3) NOT NULL,
+	hold TINYINT(1),
 	PRIMARY KEY (user_id)
 );
 
@@ -20,6 +18,7 @@ CREATE TABLE Cryptocurrency(
 	user_id INTEGER(11) NOT NULL,
 	take_profit DECIMAL(16,6),
 	stop_loss DECIMAL(16,6),
+	hold TINYINT(1),
 	PRIMARY KEY(crypto_id)
 );
 
@@ -34,30 +33,5 @@ CREATE TABLE XRP(
 	close_timestamp DATETIME,
 	quote_asset_volume DECIMAL(16,6),
 	num_trades INTEGER(11),
-	PRIMARY KEY(id)
-);
-
-CREATE TABLE XRP_SMA(
-	id INTEGER(11) NOT NULL,
-	sma_fast DECIMAL(16,6),
-	sma_slow DECIMAL(16,6),
-	PRIMARY KEY(id)
-);
-
-CREATE TABLE XRP_MACD(
-	id INTEGER(11) NOT NULL,
-	ema_fast DECIMAL(16,6),
-	ema_slow DECIMAL(16,6),
-	macd DECIMAL(16,6),
-	signal_line DECIMAL(16,6),
-	PRIMARY KEY(id)
-);
-
-CREATE TABLE XRP_ADX(
-	id INTEGER(11) NOT NULL,
-	atr DECIMAL(16,6),
-	plus_di DECIMAL(16,6),
-	minus_di DECIMAL(16,6),
-	adx DECIMAL(16,6),
 	PRIMARY KEY(id)
 );
