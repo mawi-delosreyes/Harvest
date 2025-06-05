fb06ced9c5c69ba3c10e9927c5e619b03dadf203
mawi-delosreyes <mwldelosreyes13@gmail.com>
2025-06-06 03:20:26 +0800

    Added signals

--- Simulations/Forecast_Simulation.py
+++ Simulations/Forecast_Simulation.py
@@ -1,7 +1,8 @@
 import joblib
 import numpy as np
 import warnings
-
+from Indicators.Signals import Signals
+from Simulations.Indicator_Simulation import Indicator_Simulation
 from Database.Database import Database
 from decimal import Decimal
 
@@ -17,7 +18,62 @@ class Forecast_Simulation:
 
         return Database(crypto).retrieveData(select_crypto_data_query)
     
-    
+    def getSignals(self, crypto, data):
+        sma, macd, adx, kijun, obv, rsi, close_price = Indicator_Simulation(crypto, data).runIndicators()
+        sma_short = sma[0]
+        sma_mid = sma[1]
+        sma_long = sma[2]
+        ema_fast = macd[0]
+        ema_slow = macd[1]
+        macd_value = macd[2]
+        signal_line = macd[3]
+        adx_value = adx[0]
+        atr = adx[1]
+        plus_di = adx[2]
+        minus_di = adx[3]
+        obv_value = obv[0]
+        obv_prev = obv[1]
+        rsi_value = rsi
+
+
+        # indicator_signals = Signals(sma_short, sma_mid, sma_long, ema_fast, ema_slow, macd_value, signal_line,
+        # plus_di, minus_di, adx_value, upper_band, bb_sma, lower_band,
+        # kijun, obv_value, obv_prev, pivot, r1, s1, r2, s2,
+        # r3, s3, rsi_value, close_price, forecast_start, forecast_end)
+
+        indicator_signals = Signals(sma_short, sma_mid, sma_long, ema_fast, ema_slow, macd_value, signal_line,
+        plus_di, minus_di, adx_value, None, None, None,
+        kijun, obv_value, obv_prev, None, None, None, None, None,
+        None, None, rsi_value, close_price, None, None)
+
+        weights = {
+            'SMA': 1,
+            'MACD': 2,
+            'ADX': 1.5,
+            # 'BollingerBand': 0.3,
+            # 'Kijun': 1,
+            'OBV': 0.2,
+            'RSI': 1.5
+            # 'PivotPoint': 1,
+            # 'Forecast': 2.5
+        }
+
+        indicator_signal = sum([
+            indicator_signals.SMA() * weights['SMA'], 
+            indicator_signals.MACD() * weights['MACD'], 
+            indicator_signals.ADX() * weights['ADX'], 
+            # indicator_signals.BollingerBand() * weights['BollingerBand'], 
+            # indicator_signals.Kijun() * weights['Kijun'], 
+            indicator_signals.OBV() * weights['OBV'], 
+            indicator_signals.RSI() * weights['RSI']
+            # indicator_signals.PivotPoint() * weights['PivotPoint'], 
+            # indicator_signals.Forecast() * weights['Forecast']
+        ])
+
+        # forecast_signal = indicator_signals.Forecast()
+        return indicator_signal, (sma_mid, sma_long)
+
+
     def executeBuySignal(self, crypto_price):
         risk_percent = Decimal("0.005")
         reward_percent = Decimal("0.005")
@@ -63,6 +119,8 @@ class Forecast_Simulation:
 
             btc_data = btc_window
 
+            btc_signal, btc_sma = self.getSignals("BTC", btc_data)
+
             btc_latest_data = np.array(btc_data[-n_lags:])
             features = btc_latest_data[:, [0, 1, 2, 4]]
             forecast = model.predict(features)
@@ -71,15 +129,19 @@ class Forecast_Simulation:
             max_forecast = max(forecast)
             min_forecast = min(forecast)
                 
-            crypto_price = btc_data[-1][3]
-            crypto = "BTC"
-            
+            crypto = "BTC"            
+            if crypto != None:
+                if crypto == "BTC":
+                    crypto_price = btc_data[-1][3]
+                    sma_mid = btc_sma[0]
+                    sma_long = btc_sma[1]
+
             if crypto_holdings[crypto]['cooldown'] > 0: 
                 crypto_holdings[crypto]['cooldown'] -= 1
                 print("|")
             elif crypto_holdings[crypto]['cooldown'] == 0:
                 if crypto_holdings[crypto]["hold"] == 0:
-                    if crypto_price < min_forecast:
+                    if crypto_price < min_forecast and sma_mid > sma_long:
                         tp, sl, be = self.executeBuySignal(crypto_price) 
 
                         trades += 1
